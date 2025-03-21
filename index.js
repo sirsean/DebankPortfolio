390369e59afb5891d47be1e1120228130860e295
Sean Schulte <sirsean@gmail.com>
2025-03-21 16:47:16 +0000

    morpho

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import discordClient from './src/Discord.js';
 import { Row } from './src/Row.js';
-import { totalBalance, protocolBalance, sumPortfolioBalances } from './src/Debank.js';
+import { totalBalance, protocolBalance, sumPortfolioBalances, protocolList } from './src/Debank.js';
 
 async function main() {
   const discordInitialized = await discordClient.initialize();
@@ -38,10 +38,10 @@ async function main() {
   const tokemakRow = new Row({ name: 'Tokemak', usd: tokemak.usd, eth: tokemak.eth });
   lines.push(tokemakRow.renderLine());
   await tokemakRow.publish();
-
+  
   const morpho = await Promise.all([
-    protocolBalance(process.env.WALLET_ADDRESS, 'morpho'),
-    protocolBalance(process.env.WALLET_ADDRESS, 'base_morpho'),
+    protocolBalance(process.env.WALLET_ADDRESS, 'morphoblue'),
+    protocolBalance(process.env.WALLET_ADDRESS, 'base_morphoblue'),
   ]).then(sumPortfolioBalances);
   const morphoRow = new Row({ name: 'Morpho', usd: morpho.usd });
   lines.push(morphoRow.renderLine());
